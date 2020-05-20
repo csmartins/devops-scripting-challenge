4fd23abc51ca978cfa9d662b92d701c59300cb64
Cadu Martins <csmartins.personal@gmail.com>
2020-05-20 00:30:21 -0300

    Checkout repository

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
 FROM centos:7
 
 RUN yum -y install epel-release git-core
-RUN yum -y install maven unzip java-1.8.0-openjdk python3 which
+RUN yum -y install maven unzip java-1.8.0-openjdk python3 which pip3 git
+RUN pip3 install pipenv
 
-ENV CI_PATH /opt/ci/
+ENV CI_PATH /opt/ci
 COPY src $CI_PATH
+COPY Pipfile $CI_PATH/Pipfile
+COPY Pipfile.lock $CI_PATH/Pipfile.lock
 RUN chmod +x $CI_PATH/ci.py
 ENV PATH $PATH:$CI_PATH
-RUN alias ci="$CI_PATH/ci.py"
+RUN alias ci="pipenv run $CI_PATH/ci.py"
+
+ENV LC_ALL en_US.UTF-8
+ENV LANG en_US.UTF-8
+WORKDIR $CI_PATH
+RUN pipenv install --deploy --system --ignore-pipfile
--- src/ci.py
+++ src/ci.py
@@ -1,12 +1,42 @@
 #! /usr/bin/python3
 
 import argparse
+import validators
+import logging
+import os
+import subprocess
+from git import Repo
 
-argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument("pipeline", help="The pipeline to be executed")
-argument_parser.add_argument("repo", help="The repository where to find the code")
+def checkout(repository_url, target_directory, branch=None):
+    repo = Repo.clone_from(repository_url, target_directory)
+    logging.info("Respository cloned to {}".format(target_directory))
+    
+    if branch:
+        repo.git.checkout(branch)
+        logging.info("Checked out to branch {}".branch)
+    
+    for line in os.listdir(target_directory):
+        logging.info("\t{}".format(line))
 
-arguments = argument_parser.parse_args()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("pipeline", help="The pipeline to be executed")
+    argument_parser.add_argument("repo", help="The repository where to find the code")
+    argument_parser.add_argument("-b", "--branch", required=False, help="A target branch different than master to checkout to")
 
-print(arguments.pipeline)
-print(arguments.repo)
+    arguments = argument_parser.parse_args()
+    logging.info("Continuous Integration initiated")
+    logging.debug("Loaded arguments {} and {}".format(arguments.pipeline, arguments.repo))
+
+
+    if not validators.url(arguments.repo):
+        logging.error("The given repository URL is not valid, can't work with that: {}".format(arguments.repo))
+    else:
+        logging.debug("Repository URL validated")
+        logging.info("Begin repository checkout")
+        if arguments.branch:
+            checkout(arguments.repo, "{}/checkout/".format(os.environ["CI_PATH"]), arguments.branch)
+        else:
+            checkout(arguments.repo, "{}/checkout/".format(os.environ["CI_PATH"]))
+
